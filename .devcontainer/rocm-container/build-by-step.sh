f245a2f8d9bc207759a6adbeb338b24c325e6df2
Ben Wibking <ben@wibking.com>
2025-12-29 19:33:37 -0500

    add devcontainer

---
+++ .devcontainer/rocm-container/build-by-step.sh
@@ -0,0 +1,3 @@
+docker build --platform linux/amd64 -t linux-amd64-rocm-fuzzer:main .
+#docker tag quokka-linux-amd64-rocm:development ghcr.io/quokka-astro/quokka-linux-amd64-rocm:development 
+#docker push ghcr.io/quokka-astro/quokka-linux-amd64-rocm:development 
